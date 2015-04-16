46bccdee9a4acc3887b5929bd539e0f40ae35af7
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-04-16 03:15:58 -0500

    permite editar datos de usuario desde menu clave

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -3,4 +3,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+			devise_parameter_sanitizer.for(:account_update) { |u| 
+				u.permit(:email, 
+								 :password, 
+								 :password_confirmation, 
+								 :current_password, 
+								 :nombre, 
+								 :email, 
+								 :descripcion
+								) 
+			}
+    end
 end
