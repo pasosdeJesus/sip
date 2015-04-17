e02db61fba7d446f34312367e017ea071934c26b
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-04-17 02:26:10 -0500

    usuario en lib/sip/concerns

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,29 +1,7 @@
 # encoding: UTF-8
+
+require 'sip/concerns/models/usuario'
+
 class Usuario < ActiveRecord::Base
-  @current_usuario = -1
-  attr_accessor :current_usuario
-
-  # Otros modulos de devise disponibles:
-  # :recoverable :registerable, :confirmable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :lockable
-
-  #http://stackoverflow.com/questions/1200568/using-rails-how-can-i-set-my-primary-key-to-not-be-an-integer-typed-column
-  self.primary_key=:id
-
-  def email_required?
-    false
-  end
-  validates_uniqueness_of :nusuario, :case_sensitive => false
-  validates_format_of :nusuario, :with  => /\A[a-zA-Z_0-9]+\z/
-
-  validates_presence_of :nusuario
-  validates_presence_of :idioma
-  validates_presence_of :rol
-  validates_presence_of :email
-  validates_presence_of :sign_in_count
-  validates_presence_of :fechacreacion
-
-  validates_presence_of   :encrypted_password, :on=>:create
-  validates_confirmation_of   :encrypted_password, :on=>:create
-
+  include Sip::Concerns::Models::Usuario
 end
