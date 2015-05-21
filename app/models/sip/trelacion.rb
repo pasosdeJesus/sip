f0132932ad7b37265fc6e1b99d953ffe1da1b092
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2015-05-21 12:44:00 -0600

    Solo mixins en app/models

--- app/models/sip/trelacion.rb
+++ app/models/sip/trelacion.rb
@@ -1,12 +1,9 @@
 # encoding: UTF-8
 
+require 'sip/concerns/models/trelacion'
+
 module Sip
   class Trelacion < ActiveRecord::Base
-    include Basica
-
-    has_many :persona_trelacion, foreign_key: "id_trelacion", validate: true, 
-      class_name: 'Sip::PersonaTrelacion'
-
-    validates_presence_of :inverso
+    include Sip::Concerns::Models::Trelacion
   end
 end
