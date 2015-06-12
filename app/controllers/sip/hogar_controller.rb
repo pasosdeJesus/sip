3a325a49b94c9c03945e2ef8cff3d73e53ccc7b9
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-06-12 05:31:36 -0500

    layout y hogar

--- app/controllers/sip/hogar_controller.rb
+++ app/controllers/sip/hogar_controller.rb
@@ -26,14 +26,5 @@ module Sip
       render layout: 'application'
     end
 
-    def fichacasovertical
-      if session[:capturacaso_acordeon]
-        session[:capturacaso_acordeon] = false
-      else
-        session[:capturacaso_acordeon] = true
-      end
-      redirect_to root_path
-    end
-
   end
 end
