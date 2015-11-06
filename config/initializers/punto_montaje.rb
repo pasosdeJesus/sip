604361ad16f50695ce1535c9d6aba696e6ca9c3d
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2015-11-06 12:30:36 -0500

    Refina punto de montaje

--- config/initializers/punto_montaje.rb
+++ config/initializers/punto_montaje.rb
@@ -1,6 +1,6 @@
 
-# punto de montaje de la aplicación
-Rails.application.config.relative_url_root = %q( / )
+# punto de montaje de la aplicación (no se puede /)
+Rails.application.config.relative_url_root = ''
 
 # punto de montaje de los recursos
 Rails.application.config.assets.prefix = "/assets/"
