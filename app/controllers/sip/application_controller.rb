927d032e81087912ab60a76517853b366c31a730
Vladimir Tamara Patino <vtamara@pasosdeJesus.org>
2016-05-19 04:06:47 -0500

    Soporta diversas versiones de devise

--- app/controllers/sip/application_controller.rb
+++ app/controllers/sip/application_controller.rb
@@ -22,12 +22,15 @@ module Sip
     protected
 
     def configure_permitted_parameters
-      #devise_parameter_sanitizer.for(:sign_up) << :nusuario
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nusuario])
-      #devise_parameter_sanitizer.for(:account_update) << 
-      #[:nombre, :email, :descripcion]
-      devise_parameter_sanitizer.permit(:account_update, keys: 
+      if (Gem.loaded_specs['devise'].version <=> Gem::Version.new('4.0.0')) < 0
+        devise_parameter_sanitizer.for(:sign_up) << :nusuario
+        devise_parameter_sanitizer.for(:account_update) << 
+          [:nombre, :email, :descripcion]
+      else
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:nusuario])
+        devise_parameter_sanitizer.permit(:account_update, keys: 
                                         [:nombre, :email, :descripcion])
+      end
     end
 
   end
