8b0469d60b41a1177e183ba42a44192a10564218
Vladimir Tamara <vtamara@pasosdeJesus.org>
2016-08-02 08:35:08 -0500

    creditos

--- app/models/sip/mundep.rb
+++ app/models/sip/mundep.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 
+# Ideas de https://www.sitepoint.com/speed-up-with-materialized-views-on-postgresql-and-rails/
 module Sip
   class Mundep < ActiveRecord::Base
     self.table_name = 'sip_mundep'
