5bae923402fb48cc92fb9324318951b3ed8da749
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-04-18 21:14:36 -0500

    nombre generico al editar registro de tabla basica

--- app/models/sip/usuario.rb
+++ app/models/sip/usuario.rb
@@ -5,7 +5,5 @@ require 'sip/concerns/models/usuario'
 module Sip
   class Usuario < ActiveRecord::Base
     include Sip::Concerns::Models::Usuario
-
-    validates_length_of :nombre, maximum: 50, unless: 'nombre.nil?'
   end
 end
