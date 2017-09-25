bd814795025a0b0be2c6fa4167280458da06a8ce
Vladimir Tamara <vtamara@pasosdeJesus.org>
2017-09-25 05:24:10 -0500

    Actualiza

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,23 +1,8 @@
 # encoding: UTF-8
 
 require 'sip/application_controller'
-class ApplicationController < ActionController::Base
+class ApplicationController < Sip::ApplicationController
   # Previente ataques CSRF elevando una excepción
   # En el caso de APIs, en cambio puedes querer usar :null_session
   protect_from_forgery with: :exception
-
-	before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys:
-                                        [:email, 
-                                          :password, 
-                                          :password_confirmation, 
-                                          :current_password, 
-                                          :nombre, 
-                                          :email, 
-                                          :descripcion
-      ]) 
-    end
 end
