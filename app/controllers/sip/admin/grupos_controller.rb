739d76eca676334705d6fa4730059fe4a2cd6dbc
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-03-19 15:04:03 -0500

    grupos mas genericos

--- app/controllers/sip/admin/grupos_controller.rb
+++ app/controllers/sip/admin/grupos_controller.rb
@@ -1,33 +1,13 @@
 # encoding: UTF-8
 
+require 'sip/concerns/controllers/grupos_controller'
+
 module Sip
   module Admin
     class GruposController < Sip::Admin::BasicasController
-      before_action :set_grupo, 
-        only: [:show, :edit, :update, :destroy]
+
+      include Sip::Concerns::Controllers::GruposController
       load_and_authorize_resource  class: Sip::Grupo
-  
-      def clase 
-        "Sip::Grupo"
-      end
-  
-      def set_grupo
-        @basica = Sip::Grupo.find(params[:id])
-      end
-  
-      def atributos_index
-        [ "id", "nombre"] +
-          [ :usuario_ids => [] ] +
-          ["observaciones", "fechacreacion", "fechadeshabilitacion" ]
-      end
-  
-      def genclase
-        'M'
-      end
-  
-      def grupo_params
-        params.require(:grupo).permit(*atributos_form)
-      end
   
     end
   end
---
+++ lib/sip/concerns/controllers/grupos_controller.rb
@@ -0,0 +1,44 @@
+# encoding: UTF-8
+
+module Sip
+  module Concerns
+    module Controllers
+      module GruposController
+
+        extend ActiveSupport::Concern
+
+        included do
+          include ActionView::Helpers::AssetUrlHelper
+
+          before_action :set_grupo, 
+            only: [:show, :edit, :update, :destroy]
+
+          def clase 
+            "Sip::Grupo"
+          end
+
+          def atributos_index
+            [ "id", "nombre"] +
+              [ :usuario_ids => [] ] +
+              ["observaciones", "fechacreacion", "fechadeshabilitacion" ]
+          end
+
+          def genclase
+            'M'
+          end
+
+          def set_grupo
+            @basica = Sip::Grupo.find(params[:id])
+          end
+
+          def grupo_params
+            params.require(:grupo).permit(*atributos_form)
+          end
+
+        end # include
+
+
+      end
+    end
+  end
+end
