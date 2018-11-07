95f69b5d94bd06bb490ab1474023229d1da2ef8b
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-11-07 05:46:52 -0500

    Mejores casos para basicas sin 'habilitado'

--- app/controllers/sip/admin/basicas_controller.rb
+++ app/controllers/sip/admin/basicas_controller.rb
@@ -30,6 +30,8 @@ module Sip
         r = atributos_index - ['habilitado', :habilitado]
         if !r.include?("fechadeshabilitacion_localizada") &&
           !r.include?(:fechadeshabilitacion_localizada) &&
+          !r.include?(:fechadeshabilitacion) &&
+          !r.include?('fechadeshabilitacion')
           r << "fechadeshabilitacion_localizada"
         end
         r
