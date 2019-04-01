752623cd9e4681d73b79405a9a0080b71b97c0d0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-04-01 18:01:45 -0500

    nombre restriccion

--- db/migrate/20190401175521_sin_restriccion_inverso_trelacion.rb
+++ db/migrate/20190401175521_sin_restriccion_inverso_trelacion.rb
@@ -1,7 +1,7 @@
 class SinRestriccionInversoTrelacion < ActiveRecord::Migration[5.2]
   def change
     execute <<-SQL
-      ALTER TABLE  sip_trelacion DROP CONSTRAINT IF EXISTS relacion_inverso_fkey;
+      ALTER TABLE  sip_trelacion DROP CONSTRAINT IF EXISTS trelacion_inverso_fkey;
     SQL
   end
 end
