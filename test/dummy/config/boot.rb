de5b9adfb0a59bb1182bf88c057ba18bd1bc593e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-05-26 08:34:46 -0500

    Actualiza archivos a rails6

--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -1,5 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup'  # Acelera arranque usando colchon
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 $LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
