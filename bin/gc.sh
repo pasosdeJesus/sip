927f0794c45f42cdb4eddf2ab2e5bd1b68cc6c38
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2019-06-09 13:07:00 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -28,7 +28,7 @@ if (test "$SINAC" != "1") then {
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
-	CXX=c++ yarn upgrade
+	(cd test/dummy; CXX=c++ yarn upgrade)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
