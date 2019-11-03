0c67c05372123535787153dcb7607aef4cdf902e
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-11-03 09:22:01 -0500

    Ayudadores para bootstrap 4 basados en algunos de twitter-bootsrap-rails

--- app/helpers/sip/bootstrap_helper.rb
+++ app/helpers/sip/bootstrap_helper.rb
@@ -19,8 +19,14 @@ module Sip
     module_function :menu_group
 
     # Genera opcion menú
-    def opcion_menu(opcionmenu, url)
-      r = link_to opcionmenu, url, class: 'dropdown-item' 
+    def opcion_menu(opcionmenu, url, conli=true)
+      if conli
+        r = content_tag(:li, class: 'nav-item') do
+          link_to opcionmenu, url, class: 'nav-link' 
+        end
+      else
+        r = link_to opcionmenu, url, class: 'dropdown-item' 
+      end
       return r
     end 
     module_function :opcion_menu
@@ -30,26 +36,41 @@ module Sip
 
     # Genera grupo de menus
     def despliega_abajo(opcionmenu, &bloque)
-      r = content_tag(:li, class: 'dropdown') do
-        link_to(opcionmenu, "#", class: "dropdown-toggle", 
-                "data-toggle" => "dropdown") +
-        content_tag(:ul, :class => "dropdown-menu", &bloque)
+      opab = opcionmenu.gsub(' ', '_')
+      r = content_tag(:li, class: 'nav-item dropdown') do
+        link_to(opcionmenu, '#', class: 'nav-link dropdown-toggle',
+               id: 'navbarDropdown' + opab,
+               role: 'button',
+               'data-toggle' => 'dropdown',
+               'aria-haspopup' => true,
+               'aria-expanded' => false) +
+        content_tag(:div, class: 'dropdown-menu', 
+                      'aria-labelledby' => 'navbarDropdown' + opab, &bloque)
       end
-    #  r = "<li class='nav-item dropdown'>" +
-    #  "<a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' " +
-    #  "   role='button' data-toggle='dropdown' aria-haspopup='true' " +
-    #  "   aria-expanded='false'>#{opcionmenu}</a>" +
-    #  "<div class='dropdown-menu' aria-labelledby='navbarDropdown'>" 
-    #  r2 = block_given? ? yield : nil
-    #  r += r2.nil? ? '' : r2
-    #  r += "</div>" +
-    #    "</li>"
       return r
     end 
     module_function :despliega_abajo
 
     alias_method :drop_down, :despliega_abajo
     module_function :drop_down
+
+    def nav_bar(opciones, &bloque)
+      r = content_tag(:nav, 
+                      class: 'navbar navbar-expand-lg navbar-light bg-ligt') do
+        link_to(opciones[:brand], opciones[:brand_link], class: 'navbar-brand') +
+        content_tag(:button, class: 'navbar-toggler', type: 'button',
+                   'data-toggle' => 'collapse',
+                   'data-target' => '#navbarSupportedContent',
+                   'aria-controls' => 'navbarSupportedContent',
+                   'aria-expanded' => false,
+                   'aria-label' => 'Intercambiar navegación') do
+          content_tag(:span, class: 'navbar-toggler-icon')
+        end +
+        content_tag(:div, class: 'collapse navbar-collapse', 
+                    id: 'navbarSupportedContent', &bloque)
+      end
+    end
+    module_function :nav_bar
 
   end
 end
