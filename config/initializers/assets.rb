4c1ecc59bf48a75e4994b1c1717dcc6c975731c6
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-11-11 15:51:49 -0500

    Reorganiza hojas de estilo para usar SASS de Bootstrap 4 y Fontawesome 5

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,13 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/fontawesome-free/')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/chosen-js/')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( webfonts/fa-solid-900.eot webfonts/fa-solid-900.svg webfonts/fa-solid-900.ttf chosen-sprite.png )
+
