7b930b2f5deab06dfae523fbe10cee36f12e6bda
Vladimir Tamara <vtamara@pasosdeJesus.org>
2019-12-27 09:10:16 -0500

    Mejora migracion

--- db/migrate/20191219011910_agrega_color_flotante_a_sip_tema.rb
+++ db/migrate/20191219011910_agrega_color_flotante_a_sip_tema.rb
@@ -1,6 +1,15 @@
 class AgregaColorFlotanteASipTema < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :sip_tema, :color_flota_subitem_fuente, :string
     add_column :sip_tema, :color_flota_subitem_fondo, :string
+    execute <<-SQL
+      UPDATE sip_tema SET color_flota_subitem_fuente='#266dd3',
+        color_flota_subitem_fondo='#ffffff' WHERE id='1';
+    SQL
+  end
+
+  def down
+    drop_column :sip_tema, :color_flota_subitem_fuente, :string
+    drop_column :sip_tema, :color_flota_subitem_fondo, :string
   end
 end
