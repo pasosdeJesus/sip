1e6e2632d442b16cd8cdbb8bf890a8eeec5ce198
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-03-10 05:48:41 -0500

    Renombra migracion --conflicto en cor1440_cinep

--- db/migrate/20200228235200_agrega_correo_actorsocial_persona_falta.rb
+++ db/migrate/20200228235200_agrega_correo_actorsocial_persona_falta.rb
@@ -1,4 +1,4 @@
-class AgregaCorreoActorsocialPersona < ActiveRecord::Migration[6.0]
+class AgregaCorreoActorsocialPersonaFalta < ActiveRecord::Migration[6.0]
   def up
     if !column_exists? :sip_actorsocial_persona, :correo
       add_column :sip_actorsocial_persona, :correo, :string, limit: 100
