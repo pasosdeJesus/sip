62415e62a6e0c308a50219ba3dc02a2ec1df50ae
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-06-25 15:28:46 -0500

    Lista de procesos en OpenBSD/adJ más completa

--- app/helpers/sip/procesos_helper.rb
+++ app/helpers/sip/procesos_helper.rb
@@ -6,21 +6,32 @@ module Sip
     # Retorna lista de procesos que corren en OpenBSD/adJ
     # Cada entrada es un registro con campos:
     #   pid: # de proceso
-    #   tt: abreviatur de la terminal que controla el proceso
+    #   user: usuario que lo ejecutó
+    #   ppid: # de proceso papá
+    #   pgid: # de grupo de proceso
+    #   sess: apuntador de sesión
+    #   jobc: cuenta del control de trabajos
     #   stat: estado
+    #   time: tiempo acumulado de CPU
+    #   tt: abreviatura de la terminal que controla el proceso
     #   command: orden y argumentos
     def procesos_OpenBSD
       rproc = []
-      p=`ps axww`
+      p=`ps axwwj`
       l=p.split("\n")
       l[1..-1].each do |p|
         pp = p.split(" ")
         rproc.push({
-          pid: pp[0].to_i,
-          tt: pp[1],
-          stat: pp[2],
-          time: pp[3],
-          command: pp[4..-1].join(' ')
+          user: pp[0],
+          pid: pp[1].to_i,
+          ppid: pp[2].to_i,
+          pgid: pp[3].to_i,
+          sess: pp[4].to_i,
+          jobc: pp[5].to_i,
+          stat: pp[6],
+          tt: pp[7],
+          time: pp[8],
+          command: pp[9..-1].join(' ')
         })
       end
       return rproc
