c6ff86b196d0bce93e2a886a1a98f9dea731f435
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2020-12-05 18:54:47 -0500

    Amplia funcion dif_meses_dia en ayudador Sip::FormatoFechaHelper que calcula diferencia entre fechas como meses y dias

--- app/helpers/sip/formato_fecha_helper.rb
+++ app/helpers/sip/formato_fecha_helper.rb
@@ -258,5 +258,42 @@ module Sip
       return fin
     end
     module_function :fin_semestre_ant
+
+
+    def dif_meses_dias(fechaini, fechafin) 
+      m = 0
+      d = 0
+      if fechaini && fechafin && fechaini < fechafin
+        if (fechafin.month < fechaini.month)
+          m = (fechafin.year - fechaini.year - 1)*12
+          m += 12 - fechaini.month + fechafin.month
+        else
+          m = (fechafin.year - fechaini.year)*12
+          m += fechafin.month - fechaini.month
+        end
+        if (fechafin.day < fechaini.day)
+          m = m - 1
+          d = fechaini.end_of_month.day - fechaini.day + fechafin.day
+        else
+          d = fechafin.day - fechaini.day
+        end
+        # Hasta aquí, era preciso, el siguiente tiene en cuenta lo típico
+        if (d+1) == fechafin.end_of_month.day
+          m += 1
+          d = 0
+        end
+        if (m == 0) then
+          return I18n.translate(:dia, count: d)
+        elsif (d == 0) then
+          return I18n.translate(:mes, count: m)
+        end
+        return I18n.translate(:mes, count: m) + " y " + 
+          I18n.translate(:dia, count: d)
+      else
+        return ''
+      end
+    end
+    module_function :dif_meses_dias
+
   end
 end
