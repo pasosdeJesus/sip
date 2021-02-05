614df30c836b34cec2a8ff581e384662eef69a8e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-04 21:16:50 -0500

    Actualiza

--- test/dummy/config/initializers/punto_montaje.rb
+++ test/dummy/config/initializers/punto_montaje.rb
@@ -1,4 +1,4 @@
-Sip::Application.config.relative_url_root = ENV.fetch(
+Dummy::Application.config.relative_url_root = ENV.fetch(
   'RUTA_RELATIVA', '/sip')
-Sip::Application.config.assets.prefix = ENV.fetch(
+Dummy::Application.config.assets.prefix = ENV.fetch(
   'RUTA_RELATIVA', '/sip') + '/assets'
--- test/dummy/config/initializers/sip.rb
+++ test/dummy/config/initializers/sip.rb
@@ -3,7 +3,7 @@ require 'sip/version'
 Sip.setup do |config|
   config.ruta_anexos = ENV.fetch('SIP_RUTA_ANEXOS', 
                                  "#{Rails.root}/archivos/anexos")
-  config.ruta_volcados = ENV.fetch('SIP_RUTA_VOLCADOS'],
+  config.ruta_volcados = ENV.fetch('SIP_RUTA_VOLCADOS',
                                    "#{Rails.root}/archivos/bd")
   config.titulo = "sip #{Sip::VERSION}"
   config.longitud_nusuario = 10
