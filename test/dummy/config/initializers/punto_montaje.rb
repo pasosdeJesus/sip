c2dc6b569261c5c25092415521dcc0fb8df45c39
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-03-22 05:27:22 -0500

    No inicia assets_path con /

--- test/dummy/config/initializers/punto_montaje.rb
+++ test/dummy/config/initializers/punto_montaje.rb
@@ -1,4 +1,4 @@
 Dummy::Application.config.relative_url_root = ENV.fetch(
   'RUTA_RELATIVA', '/sip')
 Dummy::Application.config.assets.prefix = ENV.fetch(
-  'RUTA_RELATIVA', '/sip') + '/assets'
+  'RUTA_RELATIVA', 'sip') + '/assets'
