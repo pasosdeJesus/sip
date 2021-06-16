f61421ed79f4874f53bcaba99b84d55b66763118
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-06-16 01:01:39 -0500

    Municipio con etiqueta

--- lib/sip/concerns/models/etiqueta.rb
+++ lib/sip/concerns/models/etiqueta.rb
@@ -9,6 +9,14 @@ module Sip
         include Sip::Basica
         included do
           self.table_name = 'sip_etiqueta'
+
+
+          has_and_belongs_to_many :municipio, 
+            class_name: 'Sip::Municipio',
+            foreign_key: 'etiqueta_id',
+            association_foreign_key: 'municipio_id',
+            join_table: 'sip_etiqueta_municipio'
+
           validates :nombre, presence: true, allow_blank: false
           validates :fechacreacion, presence: true, allow_blank: false
           validates :observaciones, length: { maximum: 500 }
