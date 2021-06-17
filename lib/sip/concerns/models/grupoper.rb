e2278cdd5263bca4f406d71ec4ce0cee368f8f15
Nombre Usuario <correo@usuario.net>
2021-06-17 08:41:23 -0500

    grupoper incluye Sip::Modelo

--- lib/sip/concerns/models/grupoper.rb
+++ lib/sip/concerns/models/grupoper.rb
@@ -7,16 +7,22 @@ module Sip
         extend ActiveSupport::Concern
 
         included do
+          include Sip::Modelo 
+          include Sip::Localizacion
+
           self.table_name = 'sip_grupoper'
 
           validates :nombre, presence: true, allow_blank: false, 
             length: { maximum: 500 }
           validates :anotaciones, length: { maximum: 500 }
 
-
           def presenta_nombre
             self.nombre
           end
+
+        end
+
+        class_methods do
 
         end
 
