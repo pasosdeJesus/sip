a86716b27df06520ecbac1cf7aa9c52ac76ff468
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-11-01 17:05:13 -0500

    Actualiza

--- test/dummy/app/packs/entrypoints/application.js
+++ test/dummy/app/packs/entrypoints/application.js
@@ -19,26 +19,19 @@
 console.log('Hola Mundo desde Webpacker')
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-// import * as ActiveStorage from "@rails/activestorage"
-// import "channels"
-
 Rails.start()
-//require('@rails/ujs').start()   // Javascript no intrusivo segun rails
+import Turbolinks from "turbolinks"
 Turbolinks.start()
-//require('turbolinks').start()   // Acelera carga de paginas
-
-//ActiveStorage.start()
 
 import $ from "expose-loader?exposes=$,jQuery!jquery";
-
+import 'jquery-ui'
+import 'jquery-ui/ui/widgets/autocomplete'
+import 'jquery-ui/ui/focusable'
+import 'jquery-ui/ui/data'
+import 'jquery-ui/ui/widgets/tooltip'
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete'
-import 'jquery-ui/ui/focusable'
-import 'jquery-ui/ui/data'
 
