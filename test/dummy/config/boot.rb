22e32ba014ba5953822158bbd473e6964eb08374
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-11-01 21:02:51 -0500

    Tras ejecutar app:update

--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -1,5 +1,5 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../Gemfile', __FILE__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../../Gemfile", __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require "bundler/setup" # Configurar gemas listadas en Gemfile
+require "bootsnap/setup" # Acelerar tiempo de arranque dejando en colchón operaciones costosas
 $LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
