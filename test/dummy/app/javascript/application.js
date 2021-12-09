0b8cef3fd966914b1bed6dc40e692ec00d055a7e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-09 12:44:45 -0500

    jquery-ui carga

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -30,15 +30,19 @@ Turbolinks.start()
 
 //ActiveStorage.start()
 
-import $ from "expose-loader?exposes=$,jQuery!jquery";
-
+import "./jquery"
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import 'jquery-ui'
-import 'jquery-ui/ui/widgets/autocomplete'
-import 'jquery-ui/ui/focusable'
-import 'jquery-ui/ui/data'
+import './jquery-ui'
+//import './jquery-ui/ui/widgets/autocomplete'
+//import './jquery-ui/ui/focusable'
+//import './jquery-ui/ui/data'
+
+$(function() {
+    $(document).tooltip()
+    $("#dialog").dialog()
+})
 
