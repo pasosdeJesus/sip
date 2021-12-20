fedf89020c91ea549f49b675bc2d258049d2aec9
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-20 11:49:02 -0500

    Actualiza

--- bin/acttodo.sh
+++ bin/acttodo.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-# Actualiza varios sistemas que usan sip. Dominio público. 2016
+# Actualiza varios sistemas que usan sip. Dominio público. 2021
 # Podria ejecutar con
-# SALTASIP=1 SALTASIPD=1 SALTAJN316=1 SALTAMR519=1 SALTAHEB412=1 
-# SALTACOR1440=1 SALTASAL7711=1 
-# SALTASIVEL2GEN=1 SALTASIVEL2=1 SALTAAPO214=1 
-# SALTASIANZORC=1 SALTASIASOM=1 SALTASIIAP=1 
-# SALTASIVELSJR=1 SALTAMIND=1 
+# SALTASIP=1 SALTAMR519=1 SALTAHEB412=1
+# SALTASIVEL2GEN=1 SALTASIVEL2=1
 
 if [ -z "$SSH_AUTH_SOCK" ] ; then
   eval `ssh-agent -s`
@@ -52,90 +49,26 @@ function actuno {
 
 if (test "$SALTASIP" != "1") then {
   echo "**** sip"
-  actuno sip test/dummy
-} fi;
-
-# Usan solo sip
-if (test "$SALTASIPD" != "1") then {
-  echo "**** sipd"
-  actuno sipd test/dummy
-} fi;
-
-
-if (test "$SALTAJN316" != "1") then {
-  echo "**** jn316"
-  actuno jn316_gen test/dummy; 
+  actuno sip-2.0 test/dummy
 } fi;
 
 if (test "$SALTAMR519" != "1") then {
   echo "**** mr519"
-  actuno mr519_gen test/dummy; 
+  actuno mr519_gen-2.0 test/dummy; 
 } fi;
 
 if (test "$SALTAHEB412" != "1") then {
   echo "**** heb412"
-  actuno heb412_gen test/dummy; 
-  actuno heb412
-} fi;
-
-if (test "$SALTACOR1440" != "1") then {
-  echo "**** cor1440"
-  actuno cor1440_gen test/dummy; 
-  actuno cor1440; 
-  actuno cor1440_pdJ; 
-  actuno si_codacop
-} fi;
-
-if (test "$SALTASAL7711" != "1") then {
-  echo "**** sal7711"
-  actuno sal7711_gen test/dummy; 
-  actuno sal7711; 
-  actuno sal7711_web test/dummy; 
+  actuno heb412_gen-2.0 test/dummy; 
 } fi;
 
 if (test "$SALTASIVEL2GEN" != "1") then {
   echo "**** sivel"
-  actuno sivel2.0_gen test/dummy; 
-  actuno sivel2_gen test/dummy; 
-} fi;
-
-if (test "$SALTAAPO214" != "1") then {
-  echo "**** apo214"
-  actuno apo214 test/dummy; 
+  actuno sivel2_gen-2.0 test/dummy; 
 } fi;
 
 if (test "$SALTASIVEL2" != "1") then {
   echo "**** sivel"
-  actuno sivel2.0; 
-  actuno sivel2
-  actuno siaddhh;
+  actuno sivel2-2.0; 
 } fi;
-
-# Usan 3 o más motores
-if (test "$SALTASIANZORC" != "1") then {
-  actuno si_anzorc; 
-} fi;
-
-if (test "$SALTASIASOM" != "1") then {
-  actuno si_asom
-} fi;
-
-if (test "$SALTASIIAP" != "1") then {
-  actuno si_iap; 
-} fi;
-
-if (test "$SALTASIVELSJR" != "1") then {
-  echo "**** sivelsjr"
-  actuno sivel2_sjr test/dummy; 
-} fi;
-
-if (test "$SALTAMIND" != "1") then {
-  echo "**** mind"
-  actuno sivel2_mujeresindigenas;
-} fi;
-
-# Usan 4 motores
-#actuno si_jrslac; 
-#actuno sivel2_sjrven; 
-actuno si_jrscol
 
