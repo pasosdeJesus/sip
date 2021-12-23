e603a4123f779989a2bfc52a069033ee9a73592d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-23 14:36:14 -0500

    Opera formulario de organización social con fechas, cocoon, autocompletación con jquery-ui y chosen

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -31,18 +31,14 @@ Turbolinks.start()
 //ActiveStorage.start()
 
 import "./jquery"
+import './jquery-ui'
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
 import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
-import './jquery-ui'
 //import './jquery-ui/ui/widgets/autocomplete'
 //import './jquery-ui/ui/focusable'
 //import './jquery-ui/ui/data'
 
-$(function() {
-    $(document).tooltip()
-    $("#dialog").dialog()
-})
-
+import '../assets/javascripts/recursos_globales'
