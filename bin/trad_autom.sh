3becba23e227114339bd9c94c57aff5390ed8b2a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-30 04:58:02 -0500

    Mejora herramienta y traducción

--- bin/trad_autom.sh
+++ bin/trad_autom.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-# Facilita traducción automática de archivos yaml con Google Translator
+# Facilita traducción automática de archivos YAML de español a inglés 
+# con Google Translator
 # vtamara@pasosdeJesus.org 2021. Dominio público
 
 fuente=$1
 if (test ! -f "$fuente") then {
-  echo "Falta archivo YAML fuente como primer argumento"
+  echo "Falta archivo YAML es fuente como primer argumento"
   exit 1;
 } fi;
 destino=$2
 if (test "$destino" = "") then {
-  echo "Falta archivo YAML destino como segundo argumento"
+  echo "Falta archivo YAML en destino como segundo argumento"
   exit 1;
 } fi;
 touch $destino
@@ -29,5 +30,16 @@ echo "Copie y pegue el resultado en /tmp/trad.txt"
 echo "Cuando lo haya hecho presione [ENTER]"
 read
 
+sed -e "s/% {/%{/g;s/& # \([0-9]*\);/\&#\1;/g;s/&\([a-z_]*\);/\&\1;/g;s/^es:$/en:/g" /tmp/trad.txt    > /tmp/trad2.txt
 
-lam /tmp/llaves.txt /tmp/trad.txt > $destino
+l1=`wc -l /tmp/trad2.txt | sed -e "s/[^0-9]*\([0-9][0-9]*\).*/\1/g"`
+l2=`wc -l /tmp/textos.txt | sed -e "s/[^0-9]*\([0-9][0-9]*\).*/\1/g"`
+while (test "$l1" -lt "$l2"); do
+  echo "" > /tmp/trad3.txt
+  cat /tmp/trad2.txt >> /tmp/trad3.txt
+  cp /tmp/trad3.txt /tmp/trad2.txt
+  l1=`wc -l /tmp/trad2.txt | sed -e "s/[^0-9]*\([0-9][0-9]*\).*/\1/g"`
+  l2=`wc -l /tmp/textos.txt | sed -e "s/[^0-9]*\([0-9][0-9]*\).*/\1/g"`
+done
+
+lam /tmp/llaves.txt /tmp/trad2.txt > $destino
