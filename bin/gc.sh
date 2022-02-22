1717a3115546fc9013d66af392c1411888340d8d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-02-22 18:09:59 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-# Revisa errore comunes, ejecuta pruebas de regresión y del sistema y envia a github 
+# Actualiza dependencias, revisa errores comunes, 
+# ejecuta pruebas y envía a repositorio
 
+if (test -f ".env") then {
+  rutaap="./"
+} elif (test -f "test/dummy/.env") then {
+  rutaap="test/dummy"
+} else {
+  echo "No se determino ruta de aplicación. Falta archivo .env"
+  exit 1;
+} fi;
+
+echo "Ruta de la aplicación: $rutaap"
 
 s=`grep -B 1 "^ *path" Gemfile 2> /dev/null`
 if (test "$?" = "0") then {
@@ -10,67 +21,74 @@ if (test "$?" = "0") then {
 
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger"
-	exit 1;
+  echo "Gemfile incluye debugger"
+  exit 1;
 } fi;
 grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
-	exit 1;
+  echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
+  exit 1;
 } fi;
 
 if (test "$SINAC" != "1") then {
-	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
-	if (test "$?" != "0") then {
-		exit 1;
-	} fi;
-	(cd test/dummy; CXX=c++ yarn upgrade)
-	if (test "$?" != "0") then {
-		exit 1;
-	} fi;
+  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
+  (cd $rutaap; CXX=c++ yarn upgrade)
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
 } fi;
 
 if (test "$SININS" != "1") then {
-	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
-	if (test "$?" != "0") then {
-		exit 1;
-	} fi;
-	(cd test/dummy; CXX=c++ yarn install)
-	if (test "$?" != "0") then {
-		exit 1;
-	} fi;
+  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
+
+  puts "\n== Enlaza controladores stimulus de motores =="
+  (cd $rutaap; bin/rails sip:stimulus_motores)
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
+
+  (cd $rutaap; CXX=c++ yarn install; bin/rails assets:precompile)
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
 } fi;
 
 if (test "$SINMIG" != "1") then {
-	(cd test/dummy; bin/rails db:migrate sip:indices db:structure:dump)
-	if (test "$?" != "0") then {
-		exit 1;
-	} fi;
+  (cd $rutaap; bin/rails db:migrate sip:indices db:schema:dump)
+  if (test "$?" != "0") then {
+    exit 1;
+  } fi;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
+(cd $rutaap; RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
 if (test "$?" != "0") then {
-	echo "No puede preparse base de prueba";
-	exit 1;
+  echo "No puede preparse base de prueba";
+  exit 1;
 } fi;
 
 CONFIG_HOSTS=www.example.com bin/rails test
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas de regresion";
-	exit 1;
+  echo "No pasaron pruebas de regresion";
+  exit 1;
 } fi;
 
-(cd test/dummy; RAILS_ENV=test bin/rails db:structure:dump)
+(cd $rutaap; RAILS_ENV=test bin/rails db:schema:dump)
 
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
-	MENSCONS="Actualiza"
+  MENSCONS="Actualiza"
 } fi;
 git commit -m "$MENSCONS" -a
 git push origin ${b}
 if (test "$?" != "0") then {
-	echo "No pudo subirse el cambio a github";
-	exit 1;
+  echo "No pudo subirse el cambio a github";
+  exit 1;
 } fi;
 
