491a9ec021755289ff4b23ace14e3c6373fbc697
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-04-20 11:09:15 -0500

    Arregla FALCÓN

--- db/migrate/20220420154535_arregla_falcon.rb
+++ db/migrate/20220420154535_arregla_falcon.rb
@@ -1,7 +1,7 @@
 class ArreglaFalcon < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
-      UPDATE sip_departamento SET nombre = 'Falcón', observaciones=NULL WHERE id=5;
+      UPDATE sip_departamento SET nombre = 'FALCÓN', observaciones=NULL WHERE id=5;
     SQL
   end
   def down
