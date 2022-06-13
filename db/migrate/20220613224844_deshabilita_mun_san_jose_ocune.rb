2575d4aa44b2116189ee23a37edb787264b8c00f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-06-13 17:55:55 -0500

    Deshabilita municipio San José de Ocune

---
+++ db/migrate/20220613224844_deshabilita_mun_san_jose_ocune.rb
@@ -0,0 +1,14 @@
+class DeshabilitaMunSanJoseOcune < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      UPDATE sip_municipio SET fechadeshabilitacion='2022-06-13',
+        observaciones='Según Wikipedia antiguamente fue un área no municipalizada pero por ordenanza fue incluida al crear el municipio de Cumaribo en 1996.'
+      WHERE id=1227;
+    SQL
+  end
+  def down
+    execute <<-SQL
+      UPDATE sip_municipio SET fechadeshabilitacion=NULL, observaciones='' WHERE id=1227;
+    SQL
+  end
+end
