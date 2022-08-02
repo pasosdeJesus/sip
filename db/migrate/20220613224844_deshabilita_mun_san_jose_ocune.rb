587776832cd30e76a85cc9715335ae1a6d4ddd2e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-02 12:01:28 -0500

    Mejora migracion

--- db/migrate/20220613224844_deshabilita_mun_san_jose_ocune.rb
+++ db/migrate/20220613224844_deshabilita_mun_san_jose_ocune.rb
@@ -4,8 +4,12 @@ class DeshabilitaMunSanJoseOcune < ActiveRecord::Migration[7.0]
       UPDATE sip_municipio SET fechadeshabilitacion='2022-06-13',
         observaciones='Según Wikipedia antiguamente fue un área no municipalizada pero por ordenanza fue incluida al crear el municipio de Cumaribo en 1996.'
       WHERE id=1227;
-      REFRESH MATERIALIZED VIEW sip_mundep;
     SQL
+    if view_exists?('sip_mundep')
+      execute <<-SQL
+        REFRESH MATERIALIZED VIEW sip_mundep;
+      SQL
+    end
   end
   def down
     execute <<-SQL
