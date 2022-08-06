6ae53662ae601adcea92aef7c096dadb6e3285f5
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-05 22:46:28 -0500

    Mejora migracion que modifica soundexes y soundexespm

--- db/migrate/20220805181901_funcion_soundexesp_multi.rb
+++ db/migrate/20220805181901_funcion_soundexesp_multi.rb
@@ -1,7 +1,7 @@
 class FuncionSoundexespMulti < ActiveRecord::Migration[7.0]
   def up
     execute <<-SQL
-      DROP FUNCTION public.soundexesp;
+      DROP FUNCTION public.soundexesp CASCADE;
       CREATE FUNCTION public.soundexesp(entrada text) RETURNS text
           LANGUAGE plpgsql IMMUTABLE STRICT COST 500
           AS $$
@@ -29,7 +29,7 @@ class FuncionSoundexespMulti < ActiveRecord::Migration[7.0]
       		entrada=translate(ltrim(trim(upper(entrada)),'H'),'ÑÁÉÍÓÚÀÈÌÒÙÜ','NAEIOUAEIOUU');
 
         IF array_upper(regexp_split_to_array(entrada, '[^a-zA-Z]'), 1) > 1 THEN
-          RAISE NOTICE 'Esta función sólo maneja una palabra. Usar soundexesp_multi para cadenas con varias palabras';
+          RAISE NOTICE 'Esta función sólo maneja una palabra y no ''%''. Use más bien soundexespm', entrada;
       		RETURN NULL;
         END IF;
 
@@ -111,22 +111,27 @@ class FuncionSoundexespMulti < ActiveRecord::Migration[7.0]
       $$;
 
 
-      DROP FUNCTION IF EXISTS public.soundexesp_multi;
-      CREATE FUNCTION public.soundexesp_multi(entrada text) RETURNS text
+      DROP FUNCTION IF EXISTS public.soundexespm CASCADE;
+      CREATE FUNCTION public.soundexespm(entrada text) RETURNS text
           LANGUAGE plpgsql IMMUTABLE STRICT COST 500
           AS $$
       DECLARE
         soundex text = '' ;
         partes text[];
         sep text = '';
+        se text = '';
       BEGIN
         entrada=translate(ltrim(trim(upper(entrada)),'H'),'ÑÁÉÍÓÚÀÈÌÒÙÜ','NAEIOUAEIOUU');
         partes=regexp_split_to_array(entrada, '[^a-zA-Z]');
 
         --raise notice 'partes=%', partes;
         FOR i IN 1 .. array_upper(partes, 1) LOOP
-          soundex = soundex || sep || soundexesp(partes[i]);
-          sep = ' ';
+          se = soundexesp(partes[i]);
+          IF length(se) > 0 THEN
+            soundex = soundex || sep || se;
+            sep = ' ';
+            --raise notice 'i=% . soundexesp=%', i, se;
+          END IF;
         END LOOP;
 
       	RETURN soundex;	
@@ -137,7 +142,124 @@ class FuncionSoundexespMulti < ActiveRecord::Migration[7.0]
   end
   def down
     execute <<-SQL
-      DROP FUNCTION IF EXISTS public.soundexesp_multi;
+
+DROP FUNCTION public.soundexesp CASCADE;
+CREATE FUNCTION public.soundexesp(input text) RETURNS text
+    LANGUAGE plpgsql IMMUTABLE STRICT COST 500
+    AS $$
+DECLARE
+	soundex text='';	
+	-- para determinar la primera letra
+	pri_letra text;
+	resto text;
+	sustituida text ='';
+	-- para quitar adyacentes
+	anterior text;
+	actual text;
+	corregido text;
+BEGIN
+       -- devolver null si recibi un string en blanco o con espacios en blanco
+	IF length(trim(input))= 0 then
+		RETURN NULL;
+	end IF;
+ 
+ 
+	-- 1: LIMPIEZA:
+		-- pasar a mayuscula, eliminar la letra "H" inicial, los acentos y la enie
+		-- 'holá coñó' => 'OLA CONO'
+        input=translate(ltrim(trim(upper(input)),'H'),'ÑÁÉÍÓÚÀÈÌÒÙÜ',
+            'NAEIOUAEIOUU');
+ 
+		-- eliminar caracteres no alfabéticos (números, símbolos como &,%,",*,!,+, etc.
+		input=regexp_replace(input, '[^a-zA-Z]', '', 'g');
+ 
+	-- 2: PRIMERA LETRA ES IMPORTANTE, DEBO ASOCIAR LAS SIMILARES
+	--  'vaca' se convierte en 'baca'  y 'zapote' se convierte en 'sapote'
+	-- un fenomeno importante es GE y GI se vuelven JE y JI; CA se vuelve KA, etc
+	pri_letra =substr(input,1,1);
+	resto =substr(input,2);
+	CASE 
+		when pri_letra IN ('V') then
+			sustituida='B';
+		when pri_letra IN ('Z','X') then
+			sustituida='S';
+		when pri_letra IN ('G') AND substr(input,2,1) IN ('E','I') then
+			sustituida='J';
+		when pri_letra IN('C') AND substr(input,2,1) NOT IN ('H','E','I') then
+			sustituida='K';
+		else
+			sustituida=pri_letra;
+ 
+	end case;
+	--corregir el parametro con las consonantes sustituidas:
+	input=sustituida || resto;		
+ 
+	-- 3: corregir "letras compuestas" y volverlas una sola
+	input=REPLACE(input,'CH','V');
+	input=REPLACE(input,'QU','K');
+	input=REPLACE(input,'LL','J');
+	input=REPLACE(input,'CE','S');
+	input=REPLACE(input,'CI','S');
+	input=REPLACE(input,'YA','J');
+	input=REPLACE(input,'YE','J');
+	input=REPLACE(input,'YI','J');
+	input=REPLACE(input,'YO','J');
+	input=REPLACE(input,'YU','J');
+	input=REPLACE(input,'GE','J');
+	input=REPLACE(input,'GI','J');
+	input=REPLACE(input,'NY','N');
+	-- para debug:    --return input;
+ 
+	-- EMPIEZA EL CALCULO DEL SOUNDEX
+	-- 4: OBTENER PRIMERA letra
+	pri_letra=substr(input,1,1);
+ 
+	-- 5: retener el resto del string
+	resto=substr(input,2);
+ 
+	--6: en el resto del string, quitar vocales y vocales fonéticas
+	resto=translate(resto,'@AEIOUHWY','@');
+ 
+    --7: convertir las letras foneticamente equivalentes a numeros  
+    --   (esto hace que B sea equivalente a V, C con S y Z, etc.)
+	resto=translate(resto, 'BPFVCGKSXZDTLMNRQJ', '111122222233455677');
+	-- así va quedando la cosa
+	soundex=pri_letra || resto;
+ 
+	--8: eliminar números iguales adyacentes (A11233 se vuelve A123)
+	anterior=substr(soundex,1,1);
+	corregido=anterior;
+ 
+	FOR i IN 2 .. length(soundex) LOOP
+		actual = substr(soundex, i, 1);
+		IF actual <> anterior THEN
+			corregido=corregido || actual;
+			anterior=actual;			
+		END IF;
+	END LOOP;
+	-- así va la cosa
+	soundex=corregido;
+ 
+	-- 9: siempre retornar un string de 4 posiciones
+	soundex=rpad(soundex,4,'0');
+	soundex=substr(soundex,1,4);		
+ 
+	-- YA ESTUVO
+	RETURN soundex;	
+END;	
+$$;
+
+
+DROP FUNCTION public.soundexespm CASCADE;
+CREATE FUNCTION public.soundexespm(in_text text) RETURNS text
+    LANGUAGE sql IMMUTABLE
+    AS $_$
+SELECT ARRAY_TO_STRING(ARRAY_AGG(soundexesp(s)),' ')
+FROM (SELECT UNNEST(STRING_TO_ARRAY(
+		REGEXP_REPLACE(TRIM($1), '  *', ' '), ' ')) AS s                
+	      ORDER BY 1) AS n;
+$_$;
+
     SQL
   end
 end
