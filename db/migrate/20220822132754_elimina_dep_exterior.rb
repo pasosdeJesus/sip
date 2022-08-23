4471f78a96e7edfe87496afec1d090dceff89e74
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-08-22 21:12:43 -0500

    Elimina departamento EXTERIOR

---
+++ db/migrate/20220822132754_elimina_dep_exterior.rb
@@ -0,0 +1,139 @@
+class EliminaDepExterior < ActiveRecord::Migration[7.0]
+  def up
+    if Sip::Clase.where(id_municipio: [828, 523, 43, 685, 323]).count > 0
+      puts "Hay centros poblados en paises codificados como muncipios"
+      exit 1
+    end
+    if Sip::Etiqueta.connection.execute('SELECT COUNT(*) FROM sip_etiqueta_municipio WHERE municipio_id IN (828, 523, 43, 685, 323)')[0]['count'] > 0
+      puts "Hay etiquetas en paises codificados como muncipios"
+      exit 1
+    end
+    if Sip::Ubicacion.
+        where(id_municipio: nil).
+        where(id_departamento: 3).count > 0
+      puts "Hay ubicaciones en EXTERIOR sin pais"
+      exit 1
+    end
+    if Sip::Ubicacionpre.
+        where(municipio_id: nil).
+        where(departamento_id: 3).count > 0
+      puts "Hay ubicacionespre en EXTERIOR sin pais"
+      exit 1
+    end
+    if Sip::Persona.
+        where(id_municipio: nil).
+        where(id_departamento: 3).count > 0
+      puts "Hay ubicacionespre en EXTERIOR sin pais"
+      exit 1
+    end
+
+
+    execute <<-SQL
+      UPDATE sip_ubicacion
+        SET id_pais=76, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=828 AND
+        id_departamento=3 AND
+        id_pais=170;
+      UPDATE sip_ubicacionpre
+        SET pais_id=76, departamento_id=NULL, municipio_id=NULL
+        WHERE municipio_id=828 AND
+        departamento_id=3 AND
+        pais_id=170;
+      UPDATE sip_persona
+        SET id_pais=76, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=828 AND
+        id_departamento=3 AND
+        id_pais=170;
+      DELETE FROM sip_municipio WHERE id=828 and nombre='Brasil';
+    SQL
+    puts "Brasil"
+
+    execute <<-SQL
+      UPDATE sip_ubicacion
+        SET id_pais=218, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=523 AND
+        id_departamento=3 AND
+        id_pais=170;
+      UPDATE sip_ubicacionpre
+        SET pais_id=218, departamento_id=NULL, municipio_id=NULL
+        WHERE municipio_id=523 AND
+        departamento_id=3 AND
+        pais_id=170;
+      UPDATE sip_persona
+        SET id_pais=218, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=523 AND
+        id_departamento=3 AND
+        id_pais=170;
+      DELETE FROM sip_municipio WHERE id=523 and nombre='Ecuador';
+    SQL
+    puts "Ecuador"
+
+    execute <<-SQL
+      UPDATE sip_ubicacion
+        SET id_pais=591, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=43 AND
+        id_departamento=3 AND
+        id_pais=170;
+      UPDATE sip_ubicacionpre
+        SET pais_id=591, departamento_id=NULL, municipio_id=NULL
+        WHERE municipio_id=43 AND
+        departamento_id=3 AND
+        pais_id=170;
+      UPDATE sip_persona
+        SET id_pais=591, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=43 AND
+        id_departamento=3 AND
+        id_pais=170;
+      DELETE FROM sip_municipio WHERE id=43 and nombre='Panama';
+    SQL
+    puts "Panama"
+
+    execute <<-SQL
+      UPDATE sip_ubicacion
+        SET id_pais=604, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=685 AND
+        id_departamento=3 AND
+        id_pais=170;
+      UPDATE sip_ubicacionpre
+        SET pais_id=604, departamento_id=NULL, municipio_id=NULL
+        WHERE municipio_id=685 AND
+        departamento_id=3 AND
+        pais_id=170;
+      UPDATE sip_persona
+        SET id_pais=604, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=685 AND
+        id_departamento=3 AND
+        id_pais=170;
+      DELETE FROM sip_municipio WHERE id=685 and nombre='Peru';
+    SQL
+    puts "Perú"
+
+    execute <<-SQL
+      UPDATE sip_ubicacion
+        SET id_pais=862, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=323 AND
+        id_departamento=3 AND
+        id_pais=170;
+      UPDATE sip_ubicacionpre
+        SET pais_id=862, departamento_id=NULL, municipio_id=NULL
+        WHERE municipio_id=323 AND
+        departamento_id=3 AND
+        pais_id=170;
+      UPDATE sip_persona
+        SET id_pais=862, id_departamento=NULL, id_municipio=NULL
+        WHERE id_municipio=323 AND
+        id_departamento=3 AND
+        id_pais=170;
+      DELETE FROM sip_municipio WHERE id=323 and nombre='Venezuela';
+    SQL
+    puts "Venezuela"
+
+    execute <<-SQL
+      DELETE FROM sip_departamento WHERE id=3 and nombre='EXTERIOR';
+    SQL
+  end
+  def down
+    raise IrreversibleMigration
+  end
+
+end
