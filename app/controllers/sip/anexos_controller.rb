bcad19a153f5352bf94a8cce59a4c02a07fdfffb
Nombre Usuario <correo@usuario.net>
2022-10-05 11:08:56 -0500

    abre anexo en lugar de descarga

--- app/controllers/sip/anexos_controller.rb
+++ app/controllers/sip/anexos_controller.rb
@@ -17,5 +17,21 @@ module Sip
         end
       end
     end
+
+    def abre_anexo
+      if !params[:id].nil?
+        @anexo = Anexo.find(params[:id].to_i)
+        ruta = @anexo.adjunto_file_name
+        if !ruta.nil?
+          # Idea para evitar inyeccion de https://www.ruby-forum.com/topic/124471
+          n=sprintf(Sip.ruta_anexos.to_s + "/%d_%s", @anexo.id.to_i, 
+                    File.basename(ruta))
+          logger.debug "Descargando #{n}"
+          send_file n, disposition: :inline
+        else
+          redirect_to usuarios_url
+        end
+      end
+    end
   end
 end
