7e2cbd29126c0423b8cc65e23f70de6aacfb43b3
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-11-03 09:44:09 -0500

    migracion menos problematica ante cambios de estructura y en motores

--- db/migrate/20210414201956_mejora_latlon_colombia.rb
+++ db/migrate/20210414201956_mejora_latlon_colombia.rb
@@ -1,14 +1,14 @@
 class MejoraLatlonColombia < ActiveRecord::Migration[6.1]
   def up
-    c=Sip::Pais.find(170)
-    c.latitud=4.7465
-    c.longitud=-75.0974
-    c.save!
+    execute <<-SQL
+      UPDATE sip_pais SET latitud=4.7465, longitud=-75.0974
+        WHERE id=170;
+    SQL
   end
   def down
-    c=Sip::Pais.find(170)
-    c.latitud=-75.0974
-    c.longitud=4.7465
-    c.save!
+    execute <<-SQL
+      UPDATE sip_pais SET latitud=-75.0974, longitud=4.7465
+        WHERE id=170;
+    SQL
   end
 end
