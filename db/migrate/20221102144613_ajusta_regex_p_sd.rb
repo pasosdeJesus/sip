80df765d8863c6050353c43c5d5465e3b4a0ecf6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-10 11:15:54 -0400

    Mejora migracion

--- db/migrate/20221102144613_ajusta_regex_p_sd.rb
+++ db/migrate/20221102144613_ajusta_regex_p_sd.rb
@@ -1,11 +1,15 @@
 class AjustaRegexPSd < ActiveRecord::Migration[7.0]
   def up
     p = Sip::Tdocumento.where(sigla: 'P').take
-    p.formatoregex = '[A-Z]*[0-9]*'
-    p.save!
+    if p
+      p.formatoregex = '[A-Z]*[0-9]*'
+      p.save!
+    end
     sd = Sip::Tdocumento.where(sigla: 'SD').take
-    sd.formatoregex = '[0-9]*[A-Z]*'
-    sd.save!
+    if sd
+      sd.formatoregex = '[0-9]*[A-Z]*'
+      sd.save!
+    end
   end
   def down
     sd = Sip::Tdocumento.where(sigla: 'SD').take
